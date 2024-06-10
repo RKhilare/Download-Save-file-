2d12338029169e3db7f6ed4986624ee390102f2c
RKhilare <roopakhilare31@inevitableelectronics.com>
2024-06-10 15:11:43 +0530

    MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.downloadfileex1;
+
+import android.Manifest;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE=100;
+    public static final String imageURl="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fphotos%2Fnature&psig=AOvVaw1ZORrzi40NAzAerPbwGKl8&ust=1718085920990000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNCNtvqu0IYDFQAAAAAdAAAAABAE";
+    String imageName="demoImage.png";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //storage runtime permission
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},REQUEST_CODE );
+            }
+        }
+
+        Button btnDownloadImage= findViewById(R.id.btnDownloadImage);
+        btnDownloadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                downloadImage(imageURl,imageName);
+            }
+        });
+    }
+    public  void downloadImage(String url,String outputFileName){
+        DownloadManager.Request request=new DownloadManager.Request(Uri.parse(url));
+        request.setTitle(imageName);
+        request.setDescription("Downloading "+ imageName);
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_ONLY_COMPLETION);
+        request.allowScanningByMediaScanner();
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,outputFileName);
+        DownloadManager manager= (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+        manager.enqueue(request);
+
+    }
+}
